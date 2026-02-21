dda0eea3095fedf475de8b59a9c10be5326d9f41
Ghost_chu <ghostchu@111.com>
2026-02-21 18:48:09 +0800

    继续更改

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,3 +1,8 @@
 const config = require('flarum-webpack-config');
 
-module.exports = config();
+const webpackConfig = config();
+
+// Disable source maps to avoid sycho/sourcemap PHP 8.x compatibility issues
+webpackConfig.devtool = false;
+
+module.exports = webpackConfig;
